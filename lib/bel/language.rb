c2d46013c092d14229b25268348fef1724497984
Anthony Bargnesi <abargnesi@selventa.com>
2013-08-05 23:30:22 -0400

    updated FUNCTIONS definition
    explicitly capture short form, long form, return type, signatures, and
    description
    
    use short form in signature strings

--- lib/bel/language.rb
+++ lib/bel/language.rb
@@ -4,215 +4,285 @@ module BEL
     FUNCTIONS = {
       abundance: {
         short_form: :a,
+        long_form: :abundance,
         description: 'Denotes the abundance of an entity',
-        signatures: ['abundance(E:A)abundance']
+        return_type: :a,
+        signatures: ['a(E:A)a']
       },
       biologicalProcess: {
         short_form: :bp,
+        long_form: :biologicalProcess,
         description: 'Denotes a process or population of events',
-        signatures: ['biologicalProcess(E:B)biologicalProcess']
+        return_type: :bp,
+        signatures: ['bp(E:B)bp']
       },
       catalyticActivity: {
         short_form: :cat,
+        long_form: :catalyticActivity,
         description: 'Denotes the frequency or abundance of events where a member acts as an enzymatic catalyst of biochecmial reactions',
+        return_type: :a,
         signatures: [
-          'catalyticActivity(F:complexAbundance)abundance',
-          'catalyticActivity(F:proteinAbundance)abundance'
+          'cat(F:complex)a',
+          'cat(F:p)a'
         ]
       },
       cellSecretion: {
         short_form: :sec,
+        long_form: :cellSecretion,
         description: 'Denotes the frequency or abundance of events in which members of an abundance move from cells to regions outside of the cells',
-        signatures: ['cellSecretion(F:abundance)abundance']
+        return_type: :a,
+        signatures: ['sec(F:a)a']
       },
       cellSurfaceExpression: {
         short_form: :surf,
+        long_form: :cellSurfaceExpression,
         description: 'Denotes the frequency or abundance of events in which members of an abundance move to the surface of cells',
-        signatures: ['cellSurfaceExpression(F:abundance)abundance']
+        return_type: :a,
+        signatures: ['surf(F:a)a']
       },
       chaperoneActivity: {
         short_form: :chap,
+        long_form: :chaperoneActivity,
         description: 'Denotes the frequency or abundance of events in which a member binds to some substrate and acts as a chaperone for the substrate',
+        return_type: :a,
         signatures: [
-          'chaperoneActivity(F:complexAbundance)abundance',
-          'chaperoneActivity(F:proteinAbundance)abundance'
+          'chap(F:complex)a',
+          'chap(F:p)a'
         ]
       },
       complexAbundance: {
         short_form: :complex,
+        long_form: :complexAbundance,
         description: 'Denotes the abundance of a molecular complex',
+        return_type: :complex,
         signatures: [
-          'complexAbundance(E:A)complexAbundance',
-          'complexAbundance(F:abundance...)complexAbundance'
+          'complex(E:A)complex',
+          'complex(F:a...)complex'
         ]
       },
       degradation: {
         short_form: :deg,
+        long_form: :degradation,
         description: 'Denotes the frequency or abundance of events in which a member is degraded in some way such that it is no longer a member',
-        signatures: ['degradation(F:abundance)abundance']
+        return_type: :a,
+        signatures: ['deg(F:a)a']
       },
       fusion: {
         short_form: :fus,
+        long_form: :fusion,
         description: 'Specifies the abundance of a protein translated from the fusion of a gene',
+        return_type: :fus,
         signatures: [
-          'fusion(E:G)fusion',
-          'fusion(E:G,E:*,E:*)fusion',
-          'fusion(E:P)fusion',
-          'fusion(E:P,E:*,E:*)fusion',
-          'fusion(E:R)fusion',
-          'fusion(E:R,E:*,E:*)fusion'
+          'fus(E:G)fus',
+          'fus(E:G,E:*,E:*)fus',
+          'fus(E:P)fus',
+          'fus(E:P,E:*,E:*)fus',
+          'fus(E:R)fus',
+          'fus(E:R,E:*,E:*)fus'
         ]
       },
       geneAbundance: {
         short_form: :g,
+        long_form: :geneAbundance,
         description: 'Denotes the abundance of a gene',
+        return_type: :g,
         signatures: [
-          'geneAbundance(E:G)geneAbundance',
-          'geneAbundance(E:G,F:fusion)geneAbundance'
+          'g(E:G)g',
+          'g(E:G,F:fus)g'
         ]
       },
       gtpBoundActivity: {
         short_form: :gtp,
+        long_form: :gtpBoundActivity,
         description: 'Denotes the frequency or abundance of events in which a member of a G-protein abundance is GTP-bound',
+        return_type: :a,
         signatures: [
-          'gtpBoundActivity(F:complexAbundance)abundance',
-          'gtpBoundActivity(F:proteinAbundance)abundance'
+          'gtp(F:complex)a',
+          'gtp(F:p)a'
         ]
       },
       kinaseActivity: {
         short_form: :kin,
+        long_form: :kinaseActivity,
         description: 'Denotes the frequency or abundance of events in which a member acts as a kinase, performing enzymatic phosphorylation of a substrate',
+        return_type: :a,
         signatures: [
-          'kinaseActivity(F:complexAbundance)abundance',
-          'kinaseActivity(F:proteinAbundance)abundance'
+          'kin(F:complex)a',
+          'kin(F:p)a'
         ]
       },
       list: {
         short_form: :list,
+        long_form: :list,
         description: 'Groups a list of terms together',
+        return_type: :list,
         signatures: [
           'list(E:A...)list',
-          'list(F:abundance...)list'
+          'list(F:a...)list'
         ]
       },
       microRNAAbundance: {
         short_form: :m,
+        long_form: :microRNAAbundance,
         description: 'Denotes the abundance of a processed, functional microRNA',
+        return_type: :m,
         signatures: [
-          'microRNAAbundance(E:M)microRNAAbundance'
+          'm(E:M)m'
         ]
       },
       molecularActivity: {
         short_form: :act,
+        long_form: :molecularActivity,
         description: 'Denotes the frequency or abundance of events in which a member acts as a causal agent at the molecular scale',
+        return_type: :a,
         signatures: [
-          'molecularActivity(F:abundance)abundance'
+          'act(F:a)a'
         ]
       },
       pathology: {
         short_form: :path,
+        long_form: :pathology,
         description: 'Denotes a disease or pathology process',
+        return_type: :path,
         signatures: [
-          'pathology(E:O)pathology'
+          'path(E:O)path'
         ]
       },
       peptidaseActivity: {
         short_form: :pep,
+        long_form: :peptidaseActivity,
         description: 'Denotes the frequency or abundance of events in which a member acts to cleave a protein',
+        return_type: :a,
         signatures: [
-          'peptidaseActivity(F:complexAbundance)abundance',
-          'peptidaseActivity(F:proteinAbundance)abundance'
+          'pep(F:complex)a',
+          'pep(F:p)a'
         ]
       },
       phosphataseActivity: {
         short_form: :phos,
+        long_form: :phosphataseActivity,
         description: 'Denotes the frequency or abundance of events in which a member acts as a phosphatase',
+        return_type: :a,
         signatures: [
-          'phosphataseActivity(F:complexAbundance)abundance',
-          'phosphataseActivity(F:proteinAbundance)abundance']
+          'phos(F:complex)a',
+          'phos(F:p)a'
+        ]
       },
       products: {
         short_form: :products,
+        long_form: :products,
         description: 'Denotes the products of a reaction',
+        return_type: :products,
         signatures: [
-          'products(F:abundance...)products'
+          'products(F:a...)products'
         ]
       },
       proteinAbundance: {
         short_form: :p,
+        long_form: :proteinAbundance,
         description: 'Denotes the abundance of a protein',
+        return_type: :p,
         signatures: [
-          'proteinAbundance(E:P)proteinAbundance',
-          'proteinAbundance(E:P,F:proteinModification)proteinAbundance',
-          'proteinAbundance(E:P,F:substitution)proteinAbundance',
-          'proteinAbundance(E:P,F:fusion)proteinAbundance',
-          'proteinAbundance(E:P,F:truncation)proteinAbundance'
+          'p(E:P)p',
+          'p(E:P,F:pmod)p',
+          'p(E:P,F:sub)p',
+          'p(E:P,F:fus)p',
+          'p(E:P,F:trunc)p'
+        ]
+      },
+      proteinModification: {
+        short_form: :pmod,
+        long_form: :proteinModification,
+        description: 'Denotes a covalently modified protein abundance',
+        return_type: :pmod,
+        signatures: [
+          'pmod(E:*)pmod',
+          'pmod(E:*,E:*)pmod',
+          'pmod(E:*,E:*,E:*)pmod'
         ]
       },
       reactants: {
         short_form: :reactants,
+        long_form: :reactants,
         description: 'Denotes the reactants of a reaction',
+        return_type: :reactants,
         signatures: [
-          'reactants(F:abundance...)reactants'
+          'reactants(F:a...)reactants'
         ]
       },
       reaction: {
         short_form: :rxn,
+        long_form: :reaction,
         description: 'Denotes the frequency or abundance of events in a reaction',
+        return_type: :a,
         signatures: [
-          'reaction(F:reactants,F:products)abundance'
+          'rxn(F:reactants,F:products)a'
         ]
       },
       ribosylationActivity: {
         short_form: :ribo,
+        long_form: :ribosylationActivity,
         description: 'Denotes the frequency or abundance of events in which a member acts to perform post-translational modification of proteins',
+        return_type: :a,
         signatures: [
-          'ribosylationActivity(F:complexAbundance)abundance',
-          'ribosylationActivity(F:proteinAbundance)abundance'
+          'ribo(F:complex)a',
+          'ribo(F:p)a'
         ]
       },
       rnaAbundance: {
         short_form: :r,
+        long_form: :rnaAbundance,
         description: 'Denotes the abundance of a gene',
+        return_type: :g,
         signatures: [
-          'rnaAbundance(E:R)geneAbundance',
-          'rnaAbundance(E:R,F:fusion)geneAbundance'
+          'r(E:R)g',
+          'r(E:R,F:fus)g'
         ]
       },
       substitution: {
         short_form: :sub,
+        long_form: :substitution,
         description: 'Indicates the abundance of proteins with amino acid substitution sequence',
+        return_type: :sub,
         signatures: [
-          'substitution(E:*,E:*,E:*)substitution'
+          'sub(E:*,E:*,E:*)sub'
         ]
       },
       transcriptionalActivity: {
         short_form: :tscript,
+        long_form: :transcriptionalActivity,
         description: 'Denotes the frequency or abundance of events in which a member directly acts to control transcription of genes',
+        return_type: :a,
         signatures: [
-          'transcriptionalActivity(F:complexAbundance)abundance',
-          'transcriptionalActivity(F:proteinAbundance)abundance'
+          'tscript(F:complex)a',
+          'tscript(F:p)a'
         ]
       },
       translocation: {
         short_form: :tloc,
+        long_form: :translocation,
         description: 'Denotes the frequency or abundance of events in which members move between locations',
+        return_type: :a,
         signatures: [
-          'translocation(F:abundance,E:A,E:A)abundance'
+          'tloc(F:a,E:A,E:A)a'
         ]
       },
       transportActivity: {
         short_form: :tport,
+        long_form: :transportActivity,
         description: 'Denotes the frequency or abundance of events in which a member directs acts to enable the directed movement of substances into, out of, within, or between cells',
+        return_type: :a,
         signatures: [
-          'transportActivity(F:complexAbundance)abundance',
-          'transportActivity(F:proteinAbundance)abundance']
+          'tport(F:)a',
+          'tport(F:p)a']
       },
       truncation: {
         short_form: :trunc,
+        long_form: :truncation,
         description: 'Indicates an abundance of proteins with truncation sequence variants',
+        return_type: :trunc,
         signatures: [
-          'truncation(E:*)truncation'
+          'trunc(E:*)trunc'
         ]
       }
     }
