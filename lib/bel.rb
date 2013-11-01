94972662d14d0c347d6a24cd475a75d29b101a2e
Anthony Bargnesi <abargnesi@selventa.com>
2013-11-01 15:06:04 -0400

    auto relative to this file's directory

--- lib/bel.rb
+++ lib/bel.rb
@@ -1,5 +1,6 @@
 # vim: ts=2 sw=2:
 module BEL
-  autoload :Language,  'bel/language'
-  autoload :Namespace, 'bel/namespace'
+  autoload :Language,  "#{File.dirname(__FILE__)}/bel/language"
+  autoload :Namespace, "#{File.dirname(__FILE__)}/bel/namespace"
+  autoload :Script,    "#{File.dirname(__FILE__)}/bel/script"
 end
