22a5577e3c6f8fce3a08e649d5050aca0e9e19b1
Anthony Bargnesi <abargnesi@selventa.com>
2015-04-28 15:07:45 -0400

    replace with parse term; updated specs

--- spec/unit/parse_ast_spec.rb
+++ spec/unit/parse_ast_spec.rb
@@ -30,25 +30,22 @@ describe BEL::Parser do
     EXPRESSIONS = {
       # simple term
       'p(HGNC:AKT1)' =>
-      'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) (null) (null) ',
+      'TERM[1][0, 12] fx(p)[0, 1] ARG[1] NV[1][2, 11] pfx(HGNC)[2, 6] val(AKT1)[7, 11] ARG[0] (null) (null) ',
       # quoted parameter
       'bp(GOBP:"apoptotic process")' =>
-      'STATEMENT SUBJECT TERM fx(bp) ARG NV pfx(GOBP) val("apoptotic process") ARG (null) (null) (null) (null) ',
+      'TERM[1][0, 28] fx(bp)[0, 2] ARG[1] NV[1][3, 27] pfx(GOBP)[3, 7] val("apoptotic process")[8, 27] ARG[0] (null) (null) ',
       # multi-parameter term
       'p(HGNC:AKT1, pmod(P, S, 300))' =>
-      'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG TERM fx(pmod) ARG NV pfx((null)) val(P) ARG NV pfx((null)) val(S) ARG NV pfx((null)) val(300) ARG (null) (null) ARG (null) (null) (null) (null) ',
+      'TERM[1][0, 26] fx(p)[0, 1] ARG[1] NV[1][2, 11] pfx(HGNC)[2, 6] val(AKT1)[7, 11] ARG[1] TERM[1][12, 25] fx(pmod)[12, 16] ARG[1] NV[1][17, 18] pfx((null)) val(P)[17, 18] ARG[1] NV[1][19, 20] pfx((null)) val(S)[19, 20] ARG[1] NV[1][21, 24] pfx((null)) val(300)[21, 24] ARG[0] (null) (null) ARG[0] (null) (null) ',
       # nested term
       'tscript(p(HGNC:AKT1))' =>
-      'STATEMENT SUBJECT TERM fx(tscript) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) ARG (null) (null) (null) (null) ',
+      'TERM[1][0, 21] fx(tscript)[0, 7] ARG[1] TERM[1][8, 20] fx(p)[8, 9] ARG[1] NV[1][10, 19] pfx(HGNC)[10, 14] val(AKT1)[15, 19] ARG[0] (null) (null) ARG[0] (null) (null) ',
       # multi-parameter nested term
       'complex(p(HGNC:AKT1), p(HGNC:AKT2), p(HGNC:AKT3))' =>
-      'STATEMENT SUBJECT TERM fx(complex) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT2) ARG (null) (null) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT3) ARG (null) (null) ARG (null) (null) (null) (null) ',
+      'TERM[1][0, 47] fx(complex)[0, 7] ARG[1] TERM[1][8, 20] fx(p)[8, 9] ARG[1] NV[1][10, 19] pfx(HGNC)[10, 14] val(AKT1)[15, 19] ARG[0] (null) (null) ARG[1] TERM[1][21, 33] fx(p)[21, 22] ARG[1] NV[1][23, 32] pfx(HGNC)[23, 27] val(AKT2)[28, 32] ARG[0] (null) (null) ARG[1] TERM[1][34, 46] fx(p)[34, 35] ARG[1] NV[1][36, 45] pfx(HGNC)[36, 40] val(AKT3)[41, 45] ARG[0] (null) (null) ARG[0] (null) (null) ',
       # long form; quoted; multi-parameter
       'translocation(p(HGNC:FOXO1),GOCCACC:"GO:0005737",GOCCACC:"GO:0005634")' =>
-      'STATEMENT SUBJECT TERM fx(translocation) ARG TERM fx(p) ARG NV pfx(HGNC) val(FOXO1) ARG (null) (null) ARG NV pfx(GOCCACC) val("GO:0005737") ARG NV pfx(GOCCACC) val("GO:0005634") ARG (null) (null) (null) (null) ',
-      # simple statement
-      'p(HGNC:AKT1) -> bp(GOBP:"apoptotic process")' =>
-      'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) (null) OBJECT REL rel(->) (null) TERM fx(bp) ARG NV pfx(GOBP) val("apoptotic process") ARG (null) (null) '
+      'TERM[1][0, 70] fx(translocation)[0, 13] ARG[1] TERM[1][14, 27] fx(p)[14, 15] ARG[1] NV[1][16, 26] pfx(HGNC)[16, 20] val(FOXO1)[21, 26] ARG[0] (null) (null) ARG[1] NV[1][28, 48] pfx(GOCCACC)[28, 35] val("GO:0005737")[36, 48] ARG[1] NV[1][49, 69] pfx(GOCCACC)[49, 56] val("GO:0005634")[57, 69] ARG[0] (null) (null) ',
     }
   end
 end
