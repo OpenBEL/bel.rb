2a39cf58ea68153321bf655629d3a9359f58b42f
Anthony Bargnesi <abargnesi@selventa.com>
2015-08-03 14:12:06 -0400

    load C extension modules when not java platform

--- lib/bel.rb
+++ lib/bel.rb
@@ -1,7 +1,4 @@
 require_relative 'bel/version'
-require_relative 'bel/libbel.rb'
-require_relative 'bel/parser'
-require_relative 'bel/completion'
 require_relative 'bel/language'
 require_relative 'bel/namespace'
 require_relative 'bel/util'
@@ -12,6 +9,12 @@ require_relative 'bel/evidence_model'
 require_relative 'bel/format'
 
 require_relative 'bel/script'
+
+unless RUBY_PLATFORM =~ /java/
+  require_relative 'bel/libbel.rb'
+  require_relative 'bel/parser'
+  require_relative 'bel/completion'
+end
 
 include BEL::Language
 include BEL::Namespace
