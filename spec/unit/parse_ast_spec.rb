26552b75b3e86c5e3178f757cb1abdd2e4d66c64
Anthony Bargnesi <abargnesi@selventa.com>
2015-08-03 20:21:22 -0400

    exclude Libbel spec tests on JRuby
    I could not figure out how to use RSpec's skip call since it needs to be
    within a describe bloc. The Libbel code needs to be prevented from being
    loaded because the C extension does not exist on JRuby.
    
    Providing an RSpec configure block with some exclusions might be better,
    but it seems to heavy-handed at the moment.

--- spec/unit/parse_ast_spec.rb
+++ spec/unit/parse_ast_spec.rb
@@ -1,58 +1,56 @@
 # These tests rely on the Ruby C extension, skip on JRuby.
-if RUBY_PLATFORM =~ /java/
-  skip
-end
+unless RUBY_PLATFORM =~ /java/
+  require 'bel'
 
-require 'bel'
+  describe BEL::Parser do
 
-describe BEL::Parser do
+    describe "#parse" do
 
-  describe "#parse" do
-
-    it "nil expressions yield nil" do
-      expect(
-        subject.parse(nil)
-      ).to eq(nil)
-    end
-
-    it "BEL expressions parse to an AST class" do
-      EXPRESSIONS.each do |term, _|
+      it "nil expressions yield nil" do
         expect(
-          subject.parse(term)
-        ).to be_a(LibBEL::BelAst)
+          subject.parse(nil)
+        ).to eq(nil)
       end
-    end
 
-    it "BEL expressions' parsed AST is correct" do
-      EXPRESSIONS.each do |term, ast_string|
-        expect(
-          LibBEL::bel_ast_as_string(subject.parse(term))
-        ).to eq(ast_string)
+      it "BEL expressions parse to an AST class" do
+        EXPRESSIONS.each do |term, _|
+          expect(
+            subject.parse(term)
+          ).to be_a(LibBEL::BelAst)
+        end
       end
-    end
 
-    EXPRESSIONS = {
-      # simple term
-      'p(HGNC:AKT1)' =>
-      'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) (null) (null) ',
-      # quoted parameter
-      'bp(GOBP:"apoptotic process")' =>
-      'STATEMENT SUBJECT TERM fx(bp) ARG NV pfx(GOBP) val("apoptotic process") ARG (null) (null) (null) (null) ',
-      # multi-parameter term
-      'p(HGNC:AKT1, pmod(P, S, 300))' =>
-      'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG TERM fx(pmod) ARG NV pfx((null)) val(P) ARG NV pfx((null)) val(S) ARG NV pfx((null)) val(300) ARG (null) (null) ARG (null) (null) (null) (null) ',
-      # nested term
-      'tscript(p(HGNC:AKT1))' =>
-      'STATEMENT SUBJECT TERM fx(tscript) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) ARG (null) (null) (null) (null) ',
-      # multi-parameter nested term
-      'complex(p(HGNC:AKT1), p(HGNC:AKT2), p(HGNC:AKT3))' =>
-      'STATEMENT SUBJECT TERM fx(complex) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT2) ARG (null) (null) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT3) ARG (null) (null) ARG (null) (null) (null) (null) ',
-      # long form; quoted; multi-parameter
-      'translocation(p(HGNC:FOXO1),GOCCACC:"GO:0005737",GOCCACC:"GO:0005634")' =>
-      'STATEMENT SUBJECT TERM fx(translocation) ARG TERM fx(p) ARG NV pfx(HGNC) val(FOXO1) ARG (null) (null) ARG NV pfx(GOCCACC) val("GO:0005737") ARG NV pfx(GOCCACC) val("GO:0005634") ARG (null) (null) (null) (null) ',
-      # simple statement
-      'p(HGNC:AKT1) -> bp(GOBP:"apoptotic process")' =>
-      'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) (null) OBJECT REL rel(->) (null) TERM fx(bp) ARG NV pfx(GOBP) val("apoptotic process") ARG (null) (null) '
-    }
+      it "BEL expressions' parsed AST is correct" do
+        EXPRESSIONS.each do |term, ast_string|
+          expect(
+            LibBEL::bel_ast_as_string(subject.parse(term))
+          ).to eq(ast_string)
+        end
+      end
+
+      EXPRESSIONS = {
+        # simple term
+        'p(HGNC:AKT1)' =>
+        'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) (null) (null) ',
+        # quoted parameter
+        'bp(GOBP:"apoptotic process")' =>
+        'STATEMENT SUBJECT TERM fx(bp) ARG NV pfx(GOBP) val("apoptotic process") ARG (null) (null) (null) (null) ',
+        # multi-parameter term
+        'p(HGNC:AKT1, pmod(P, S, 300))' =>
+        'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG TERM fx(pmod) ARG NV pfx((null)) val(P) ARG NV pfx((null)) val(S) ARG NV pfx((null)) val(300) ARG (null) (null) ARG (null) (null) (null) (null) ',
+        # nested term
+        'tscript(p(HGNC:AKT1))' =>
+        'STATEMENT SUBJECT TERM fx(tscript) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) ARG (null) (null) (null) (null) ',
+        # multi-parameter nested term
+        'complex(p(HGNC:AKT1), p(HGNC:AKT2), p(HGNC:AKT3))' =>
+        'STATEMENT SUBJECT TERM fx(complex) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT2) ARG (null) (null) ARG TERM fx(p) ARG NV pfx(HGNC) val(AKT3) ARG (null) (null) ARG (null) (null) (null) (null) ',
+        # long form; quoted; multi-parameter
+        'translocation(p(HGNC:FOXO1),GOCCACC:"GO:0005737",GOCCACC:"GO:0005634")' =>
+        'STATEMENT SUBJECT TERM fx(translocation) ARG TERM fx(p) ARG NV pfx(HGNC) val(FOXO1) ARG (null) (null) ARG NV pfx(GOCCACC) val("GO:0005737") ARG NV pfx(GOCCACC) val("GO:0005634") ARG (null) (null) (null) (null) ',
+        # simple statement
+        'p(HGNC:AKT1) -> bp(GOBP:"apoptotic process")' =>
+        'STATEMENT SUBJECT TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG (null) (null) (null) OBJECT REL rel(->) (null) TERM fx(bp) ARG NV pfx(GOBP) val("apoptotic process") ARG (null) (null) '
+      }
+    end
   end
 end
