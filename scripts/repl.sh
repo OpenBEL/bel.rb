92dde271549c4962f7daab975e649be0a417e9d0
Anthony Bargnesi <abargnesi@selventa.com>
2015-10-21 13:35:50 -0400

    repl script; run "rake compile" first

--- scripts/repl.sh
+++ scripts/repl.sh
@@ -4,5 +4,14 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 source "$DIR"/env.sh || exit 1
 cd "$DIR" || exit 1
 
+echo "Compiling libbel C extension."
+COMPILE_OUT=$(mktemp)
+rake compile > /dev/null 2> $COMPILE_OUT
+if [ $? -ne 0 ]; then
+  echo "Compilation error, output of \"rake compile\":"
+  cat $COMPILE_OUT
+  exit 1
+fi
+
 pry -I "./lib" -r "bel"
 
