17bb72e1147dabdf7e1d08844607c315297cd65c
Nick Bargnesi <nbargnesi@selventa.com>
2015-10-28 16:56:54 -0400

    bump gem dependency file, gem.deps.rb
    Include reference documentation URLs.

--- gem.deps.rb
+++ gem.deps.rb
@@ -1,7 +1,11 @@
-puts 'Installing bel.rb development gems.'
+# See the following for reference:
+# - http://docs.ruby-lang.org/en/2.2.0/Gem/RequestSet/GemDependencyAPI.html
+# - http://docs.ruby-lang.org/en/2.2.0/Gem/RequestSet/GemDependencyAPI.html#method-i-group
+# - http://docs.ruby-lang.org/en/2.2.0/Gem/RequestSet/GemDependencyAPI.html#method-i-platform
+# - http://docs.ruby-lang.org/en/2.2.0/Gem.html#method-c-use_gemdeps
 source 'https://rubygems.org'
 
-# gems used in development
+# Gems used in development.
 gem 'minitest', '~> 5.7'
 gem 'rake', '~> 10.4'
 gem 'rake-compiler', '~> 0.9'
@@ -13,8 +17,13 @@ gem 'pry-byebug', '~> 3.2'
 gem 'rubocop', '~> 0.34'
 gem 'rantly', '~> 0.3'
 
-# for testing RDF functionality
+# For testing RDF functionality.
 gem 'addressable', '~> 2.3'
 gem 'rdf', '~> 1.1'
 gem 'rdf-turtle', '~> 1.1'
 gem 'uuid', '~> 2.3'
+
+# Platform-specific example.
+# platform(:jruby) {
+#     gem 'json-jruby', '~> 1.5'
+# }
