ee359ba42819963366a683202c0060f6bdcaa25b
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-29 01:11:48 -0400

    reorganized versioning/gemspec
    motivation is based on my view of current best practice for gem
    management; influenced by the ruby-rdf/rdf gems
    
    moved version to VERSION file in root; beef'd up BEL::Version with more
    semantic goodness
    
    moved the gemspec to .gemspec; symlinked bel.gemspec to .gemspec

--- lib/bel/version.rb
+++ lib/bel/version.rb
@@ -1,3 +1,33 @@
 module BEL
-  VERSION = '0.7.0'
+  # Captures the semantic version of the library.
+  module Version
+    # The frozen version {String}. See {Object#freeze}.
+    STRING =
+      File.read(
+        File.join(
+          File.expand_path(File.dirname(__FILE__)), '..', '..', 'VERSION'
+        )).chomp.freeze
+
+    # The frozen {Fixnum version numbers}. See {Object#freeze}.
+    MAJOR, MINOR, PATCH = STRING.split('.').map(&:freeze)
+    # The frozen {Array} of {Fixnum version numbers}. See {Object#freeze}.
+    VERSION_NUMBERS     = [MAJOR, MINOR, PATCH].freeze
+
+    # Add singleton methods to the metaclass of {BEL::Version}.
+    class << self
+
+      # Return the frozen, semantic version {String}.
+      # @return [frozen String] the semantic version
+      def to_s
+        STRING
+      end
+      alias :to_str :to_s
+
+      # Return the frozen, semantic version number {Array}.
+      # @return [frozen Array] the semantic version numbers
+      def to_a
+        VERSION_NUMBERS
+      end
+    end
+  end
 end
