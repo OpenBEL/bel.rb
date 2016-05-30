d18626c830df6545c0e610510ceb5b42f6185d41
Nick Bargnesi <nbargnesi@selventa.com>
2016-05-30 17:58:33 -0400

    bump ci-test support (unit tests only)

--- scripts/ci-unit-tests.sh
+++ scripts/ci-unit-tests.sh
@@ -16,11 +16,8 @@ source "$BR_SCRIPTS"/isolate.sh || exit 1
 "$BR_SCRIPTS"/gem-install-devdeps.sh || exit 1
 
 rake compile || exit 1
-rspec spec/unit --format RspecJunitFormatter --out test-output.xml $@
-RSPEC_RC=$?
-if [ $RSPEC_RC -eq 0 ]; then
-    echo "You totes win. Epic."
-else
-    echo "FAIL"
-fi
+find spec/unit -name "*.rb" | while read unittest; do
+    report="test/reports/$(basename $unittest)-junit.xml"
+    rspec --format RspecJunitFormatter --out "$report" "$unittest" $@
+done
 exit 0
